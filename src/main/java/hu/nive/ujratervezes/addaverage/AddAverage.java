6c88159246e8ff1993cd2752ed6ad95816a8dff9
bakroli <bak.roland@hotmail.com>
2022-02-28 15:05:06 +0100

    Finish

--- src/main/java/hu/nive/ujratervezes/addaverage/AddAverage.java
+++ src/main/java/hu/nive/ujratervezes/addaverage/AddAverage.java
@@ -3,6 +3,30 @@ package hu.nive.ujratervezes.addaverage;
 public class AddAverage {
 
     public int[] addAverage(int[] numbers, int n) {
+        if (numbers == null || n < 1) {
+            throw new IllegalArgumentException();
+        }
+        if (numbers.length == 0) {
+            return new int[0];
+        }
+        int[] result = new int[numbers.length];
+        int average = averageCalculator(numbers);
+        for (int i = 0; i < numbers.length; i++) {
+            if ((i + 1) % n == 0) {
+                result[i] = numbers[i] * average;
+            } else {
+                result[i] = numbers[i];
+            }
+        }
+        return result;
+    }
+
+    private int averageCalculator(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum / numbers.length;
     }
 
     public static void main(String[] args) {
